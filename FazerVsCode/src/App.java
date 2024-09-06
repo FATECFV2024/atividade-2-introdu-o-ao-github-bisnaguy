6b1f014de35cafd09ab4d5646bafacbaae85804e
Gabriel Azambuja Silva <azambujagabriel95@gmail.com>
2024-09-05 22:49:58 -0300

    [feat]Primeiro commit dos meus dados

--- FazerVsCode/src/App.java
+++ FazerVsCode/src/App.java
@@ -1,29 +1,10 @@
-import java.util.Scanner;
-
 public class App {
-        //Comentário
-
-    public static void funcao_teste (){
-        //Biblioteca para entrada de dados
-        Scanner ler = new Scanner(System.in);
-
-        //variável local 
-        String v2;
-        int v1;
-
-        System.out.println("Comando de Saida de Dados");
-        
-        System.out.printf("Informe uma palavra: ");
-        v2 = ler.next();
-
-        System.out.printf("Informe um número: ");
-        v1 = ler.nextInt();
-
-        System.out.printf(v2+" "+v1);
-    }
-    public static void main(String[] args) throws Exception {
-        funcao_teste();
-
-
-    }
+    public static void main(String[] args) {
+        System.out.println("Meu nome completo é Gabriel Azambuja Silva");
+        System.out.println("Meu RA é 2920482411049");
+        System.out.println("Tenho 26 anos");
+        System.out.println("Meu perfil no GitHub é https://github.com/bisnaguy");
+        System.out.println("Meu perfil no Linkedin é https://www.linkedin.com/in/gabriel-azambuja-373397173/");
+        System.out.println("Estou cursando Análise e Desenvolvimento de Sistemas na Fatec Ferraz de Vasconcelos, atualmente curso o Segundo semestre no ano de 2024");
+    }        
 }
